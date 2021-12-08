a38b48443d9962cfc2a77daeb5e654a3ebcf2d1e
Régis Haubourg <regis.haubourg@cstb.fr>
2021-12-08 18:11:17 +0100

    adds hour and times to backup_dir name

--- pg_backup_rotated.sh
+++ pg_backup_rotated.sh
@@ -64,7 +64,7 @@ fi;
 function perform_backups()
 {
 	SUFFIX=$1
-	FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d`$SUFFIX/"
+	FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%dT\%H:\%M-$SUFFIX/`"
 
 	echo "Making backup directory in $FINAL_BACKUP_DIR"
 
